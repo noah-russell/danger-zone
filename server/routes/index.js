aa05c91c7af0e28ba9eabb994912a57d9436effe
Keeley Owen <keeleytowen@gmail.com>
2023-10-20 13:15:59 +1300

    change routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
+import * as path from 'node:path'
 import express from 'express'
 const router = express.Router()
 
 // router.get('/', (req, res) => {
 //   res.render('main', { hi: 'Hello World!' })
 // })
+
+router.get('/race1', (req, res) => {
+  res.render('race')
+})
 
 export default router
