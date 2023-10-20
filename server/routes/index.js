6238f5c0e1e68f961a81dcb472849afeeca6885b
Keeley Owen <keeleytowen@gmail.com>
2023-10-20 13:31:14 +1300

    route work

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,23 @@ const router = express.Router()
 //   res.render('main', { hi: 'Hello World!' })
 // })
 
-router.get('/race1', (req, res) => {
+router.get('/race1', async (req, res) => {
+  // let jsonData = await fsPromises.readFile('server/data/data.json', 'utf-8')
+  // let puppyData = JSON.parse(jsonData)
   res.render('race')
 })
+
+// router.get('/:id', async (req, res) => {
+//   // JSON format is convenient for reading and writing
+//   let jsonData = await fsPromises.readFile('server/data/data.json', 'utf-8')
+//   let puppyData = JSON.parse(jsonData)
+
+//   let singlePuppy = puppyData.puppies.find(
+//     (element) => element.id === Number(req.params.id)
+//   )
+
+//   // console.log(singlePuppy)
+//   res.render('details', singlePuppy)
+// })
 
 export default router
