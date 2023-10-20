0166423fb70c95ac5d3614b7a51f70e99acc9cb5
Keeley Owen <keeleytowen@gmail.com>
2023-10-20 14:02:08 +1300

    add routes for win and lose

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,13 @@ router.get('/race', (req, res) => {
   res.render('race')
 })
 
+router.get('/loseScreen', (req, res) => {
+  res.render('loseScreen')
+})
+
+router.get('/winScreen', (req, res) => {
+  res.render('winScreen')
+})
 // router.get('/:id', async (req, res) => {
 //   // JSON format is convenient for reading and writing
 //   let jsonData = await fsPromises.readFile('server/data/data.json', 'utf-8')
